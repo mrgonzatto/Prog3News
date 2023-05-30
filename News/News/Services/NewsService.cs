a6e5695e2d96521728109b59d173b9fe832ee217
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2023-05-30 20:44:38 -0300

    Http header user agent

--- News/News/Services/NewsService.cs
+++ News/News/Services/NewsService.cs
@@ -13,6 +13,8 @@ namespace News.Services
             string url = GetUrl(scope);
 
             var webclient = new WebClient();
+            webclient.Headers["User-Agent"] = 
+                "Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0) (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)";
             string ret = string.Empty;
 
             try
